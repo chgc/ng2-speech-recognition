b31818d9afe9584d2802492a2ab91139606c1179
Kevin Yang <Kevin Yang>
2016-09-30 20:13:46 +0800

    加上animation

--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -1,10 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-current',
   templateUrl: './current.component.html',
-  styleUrls: ['./current.component.css']
+  styleUrls: ['./current.component.css'],
+  animations: [
+    trigger('visibilityChanged', [
+      state('shown', style({ opacity: 1 })),
+      state('hidden', style({ opacity: 0 })),
+      transition('shown => hidden', animate('500ms')),
+      transition('hidden => shown', animate('300ms')),
+    ])
+  ]
 })
 export class CurrentComponent implements OnInit {
 
